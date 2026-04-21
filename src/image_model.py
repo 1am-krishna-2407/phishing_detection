b696f975963b4d0b4a6b982972d79b6f525133ac
Krishna Jha <117565242+1am-krishna-2407@users.noreply.github.com>
2026-04-21 09:51:00 +0530

    Remove pretrained weights from ResNet18 model

--- src/image_model.py
+++ src/image_model.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
-from torchvision.models import ResNet18_Weights, resnet18
+from torchvision.models import resnet18
 
 
 class ImagePhishingModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        self.model = resnet18(weights=None)
         self.model.fc = nn.Linear(512, 1)
 
     def forward(self, x):
